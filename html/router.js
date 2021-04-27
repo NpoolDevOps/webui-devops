08702f509a0997302dba45321c5272dc76b605a3
crayzpl <crazyzplzpl@gmail.com>
2021-04-27 10:35:39 +0800

    update

--- html/router.js
+++ html/router.js
@@ -1,8 +1,8 @@
 const devops = httpVueLoader('./main/devops.vue')
 const devopsdt = httpVueLoader('./main/devopsdt.vue')
 const routes = [
-    { path: '/', component: devops },
-    { path: '/:index', component: devopsdt }
+    { path: '/web/ui/', component: devops },
+    { path: 'web/ui/:index', component: devopsdt }
 ]
 const router = new VueRouter({
     routes: routes,
