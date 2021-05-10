75a3205356ea24f7b5dff1e9aed91ad974a83ef6
crayzpl <crazyzplzpl@gmail.com>
2021-05-10 17:50:30 +0800

    update: 简单构建了一下设备详情页面

--- html/main/device-detail/devopsDeviceDetail.vue
+++ html/main/device-detail/devopsDeviceDetail.vue
@@ -1,15 +1,79 @@
 <template>
-  <div>
-    <span>这里是详细页面</span>
-  </div>
+<div class="detail-card-style">
+  <span class="title-style">device number</span>
+  <el-card class="card-style">
+    <span>设备概览</span>
+    <div class="device-style">
+      <div class="div-device-fee">
+        <span>账户余额：10000</span><br />
+        <span>可用余额</span><br />
+        <span>扇区抵押</span><br />
+        <!--下方插入折线图直观展示fee变化-->
+      </div>
+
+      <div class="div-device-power">
+        <span>有效算力</span><br />
+        <span>设备状态</span>
+        <!--下方插入折线图直观展示power变化-->
+      </div>
+    </div>
+  </el-card>
+    
+  <el-card class="card-style">
+    <span>统计</span>
+    <div class="device-style">
+      <ul>
+        <li v-for="i in 4">{{i}}</li>
+      </ul>
+    </div>
+  </el-card>
+
+  <el-card class="card-style">
+    <span>账户信息</span>
+    <div class="device-style">
+      <ul>
+        <li v-for="i in 4">{{i}}</li>
+      </ul>
+    </div>
+  </el-card>
+</div>
 </template>
 
 <script>
 module.exports = {
-  
+
 }
 </script>
 
 <style scoped>
+.detail-card-style {
+  margin: 20px;
+}
 
+.title-style {
+  font-size: 18px;
+  border-bottom: 1px solid gray;
+}
+
+.card-style {
+  margin-top: 30px;
+}
+
+.device-style {
+  display: flex;
+  flex-direction: row;
+  padding: 10px;
+  margin: 20px;
+}
+
+.div-device-fee {
+  box-shadow: 10px 10px 40px rgba(0, 0, 0, 0.1);
+  width: 750px;
+}
+
+.div-device-power {
+  box-shadow: 10px 10px 40px rgba(0, 0, 0, 0.1);
+  width: 750px;
+  margin-left: 60px;
+}
 </style>
--- html/main/devopscomponents/DevopsAllList.vue
+++ html/main/devopscomponents/DevopsAllList.vue
@@ -1,114 +1,72 @@
 <template>
-  <div class="card-list" :v-if="cardMinerDevices.length > 0">
-    <el-row gutter="20">
-      <el-col
-        span="7"
-        class="card-margin"
-        v-for="(device, index) in cardMinerDevices"
-        :key="index"
-      >
-        <el-card class="card-list-style" shadow="hover">
-          <div slot="header">
-            <el-button
-              :id="'/device' + device.device.pathIndex"
-              type="text"
-              class="title-btn"
-              @click="goToDetail($event)"
-              >{{ device.device.local_addr }}</el-button
-            >
-            <div class="reminder-light">
-              <div class="outer">
-                <div class="inner"></div>
-              </div>
+<div class="card-list" :v-if="cardMinerDevices.length > 0">
+  <el-row gutter="20">
+    <el-col span="7" class="card-margin" v-for="(device, index) in cardMinerDevices" :key="index">
+      <el-card class="card-list-style" shadow="hover">
+        <div slot="header">
+          <el-button :id="'/device' + device.device.pathIndex" type="text" class="title-btn" @click="goToDetail($event)">{{ device.device.local_addr }}</el-button>
+          <div class="reminder-light">
+            <div class="outer">
+              <div class="inner"></div>
             </div>
           </div>
-          <el-row gutter="24" class="up-info-style">
-            <el-col
-              :span="8"
-              v-for="(value1, name1, index1) in device.device"
-              class="info-position"
-            >
-              <span
-                class="title-font-style"
-                v-if="name1 != 'pathIndex' && name1 != 'role'"
-                >{{ $t(`device[${parseInt(index1)}]`) }}:</span
-              >
+        </div>
+        <el-row gutter="24" class="up-info-style">
+          <el-col :span="8" v-for="(value1, name1, index1) in device.device" class="info-position">
+            <span class="title-font-style" v-if="name1 != 'pathIndex' && name1 != 'role'">{{ $t(`device[${parseInt(index1)}]`) }}:</span>
+            <br />
+            <span class="card-content" v-if="name1 != 'pathIndex' && name1 != 'role'">{{ value1 }}</span>
+          </el-col>
+        </el-row>
+        <el-row gutter="20">
+          <el-col :span="8" class="pie-position">
+            <div class="card-chart">
+              <pie-chart-fee class="pie-chart" :send-miner-device="device.device" :send-miner-info="device.info"></pie-chart-fee>
+            </div>
+          </el-col>
+          <el-col :span="12" class="info-display">
+            <div class="fee-power-text-position">
+              <span class="title-font-style">{{ $t("getMiner.getMinerFee.miner_balance") }}:</span>
               <br />
-              <span
-                class="card-content"
-                v-if="name1 != 'pathIndex' && name1 != 'role'"
-                >{{ value1 }}</span
-              >
-            </el-col>
-          </el-row>
-          <el-row gutter="20">
-            <el-col :span="8" class="pie-position">
-              <div class="card-chart">
-                <pie-chart-fee
-                  class="pie-chart"
-                  :send-miner-device="device.device"
-                  :send-miner-info="device.info"
-                ></pie-chart-fee>
-              </div>
-            </el-col>
-            <el-col :span="12" class="info-display">
-              <div class="fee-power-text-position">
-                <span class="title-font-style"
-                  >{{ $t("getMiner.getMinerFee.miner_balance") }}:</span
-                >
-                <br />
-                <span class="card-content">{{
+              <span class="card-content">{{
                   device.info.getMinerFee.miner_balance
                 }}</span>
-              </div>
-            </el-col>
-          </el-row>
+            </div>
+          </el-col>
+        </el-row>
 
-          <el-row gutter="20">
-            <el-col :span="8" class="pie-position">
-              <div class="card-chart">
-                <pie-chart-power
-                  class="pie-chart"
-                  :send-miner-device="device.device"
-                  :send-miner-info="device.info"
-                ></pie-chart-power>
-              </div>
-            </el-col>
-            <el-col :span="12" class="info-display">
-              <div class="fee-power-text-position">
-                <span class="title-font-style"
-                  >{{ $t("getMiner.getMinerPower.miner_power") }}/{{
+        <el-row gutter="20">
+          <el-col :span="8" class="pie-position">
+            <div class="card-chart">
+              <pie-chart-power class="pie-chart" :send-miner-device="device.device" :send-miner-info="device.info"></pie-chart-power>
+            </div>
+          </el-col>
+          <el-col :span="12" class="info-display">
+            <div class="fee-power-text-position">
+              <span class="title-font-style">{{ $t("getMiner.getMinerPower.miner_power") }}/{{
                     $t("getMiner.getMinerPower.miner_faulty_power")
-                  }}:</span
-                >
-                <br />
-                <span class="card-content">{{
+                  }}:</span>
+              <br />
+              <span class="card-content">{{
                   device.info.getMinerPower.miner_power
-                }}</span
-                >/<span class="card-content faulty-color" style="padding: 0px;">{{
+                }}</span>/<span class="card-content faulty-color" style="padding: 0px;">{{
                   device.info.getMinerPower.miner_faulty_power
                 }}</span>
-              </div>
-            </el-col>
-          </el-row>
+            </div>
+          </el-col>
+        </el-row>
 
-          <div class="card-carousel">
-            <el-carousel
-              :interval="3000"
-              height="50px"
-              direction="vertical"
-              autoplay="true"
-              indicator-position="none"
-            >
-              <el-carousel-item v-for="item in 4" :key="item">
-                <h3>{{ item }}</h3>
-              </el-carousel-item>
-            </el-carousel>
-          </div>
-        </el-card>
-      </el-col>
-    </el-row>
-  </div>
+        <div class="card-carousel">
+          <el-carousel :interval="3000" height="50px" direction="vertical" autoplay="true" indicator-position="none">
+            <el-carousel-item v-for="item in 4" :key="item">
+              <h3>{{ item }}</h3>
+            </el-carousel-item>
+          </el-carousel>
+        </div>
+      </el-card>
+    </el-col>
+  </el-row>
+</div>
 </template>
 
 <script>
@@ -186,7 +144,8 @@ module.exports = {
   display: inline-block;
 }
 
-.title-btn:before,.title-btn:after {
+.title-btn:before,
+.title-btn:after {
   content: "";
   display: block;
   width: 0;
@@ -201,6 +160,7 @@ module.exports = {
   transition: border-color 0s ease-in 0.8s, width 0.2s ease-in 0.6s,
     height 0.2s ease-in 0.4s;
 }
+
 .title-btn:after {
   right: 0;
   bottom: 0;
@@ -214,7 +174,7 @@ module.exports = {
   transition: border-color 0s ease-in 0.4s, width 0.2s ease-in 0.4s,
     height 0.3s ease-in 0.6s;
   border-bottom-color: #67c23a;
-  
+
 }
 
 .reminder-light {
