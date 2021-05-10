97a62697344eacabe24d7f657f9777688791e106
crayzpl <crazyzplzpl@gmail.com>
2021-05-10 15:02:51 +0800

    update: 稍微完善了setting页面

--- html/main/setting.vue
+++ html/main/setting.vue
@@ -1,11 +1,47 @@
 <template>
-  <span>这里是设置页面</span>
+  <div>
+    <el-card class="card-style">
+      <div class="setting-box">
+        <span class="setting-title">用户信息更改</span>
+        <set-user-info></set-user-info>
+      </div>
+      <div class="setting-box">
+        <span class="setting-title">设备选择</span>
+        <set-transfer-device></set-transfer-device>
+      </div>
+      <div class="setting-box">
+        <span class="setting-title">多选设备</span>
+        <set-checkbox></set-checkbox>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script>
-  module.exports = {
-    data(){
-      return {} 
-    }
-  }
+module.exports = {
+  components: {
+    SetUserInfo: httpVueLoader("../tool/setting/SetUserInfo.vue"),
+    SetTransferDevice: httpVueLoader("../tool/setting/SetTransferDevice.vue"),
+    SetCheckbox: httpVueLoader('../tool/setting/SetCheckbox.vue'),
+  },
+};
 </script>
+
+<style scoped>
+.card-style {
+  margin: 10px 30px 0px 20px;
+}
+
+.setting-box {
+  margin-left: 350px;
+  margin-bottom: 20px;
+}
+
+.setting-title {
+  font-size: 16px;
+  color: gray;
+  border-bottom: 2px solid gray;
+  width: 100%;
+  margin-left: 50px;
+}
+</style>
---
+++ html/tool/setting/SetTransferDevice.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <el-transfer v-model="value" :data="devices" class="transfer-style" :titles="['所有设备', '问题设备']">
+    </el-transfer>
+    <el-button @click="onValue">查看Value</el-button>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  data() {
+    const generateData = (_) => {
+      const data = [];
+      for (let i = 1; i <= 15; i++) {
+        data.push({
+          key: i,
+          label: `备选项 ${i}`,
+          // deviceNum: i.toString(),
+        });
+      }
+      return data;
+    };
+    return {
+      devices: generateData(),
+      value: [1, 4], //每一个data的key
+    };
+  },
+
+  methods: {
+    onValue: function () {
+      console.log("value:", this.value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.transfer-style {
+  margin: 30px 0px 0px 150px;
+}
+</style>
