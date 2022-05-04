8851583c0c54adc626890e11ccbdfc2c1d015a8f
Zhao KK <kikakkz@hotmail.com>
2022-05-04 18:29:37 +0800

    Add timeout

--- html/main/devops.vue
+++ html/main/devops.vue
@@ -73,6 +73,7 @@ module.exports = {
         data: {
           auth_code: authCode,
         },
+	timeout: 600000,
       })
         .then(function (response) {
           let resp = response.data;
